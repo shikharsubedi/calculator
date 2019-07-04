102db68f53066d4e63297cbc5c21696772c67d29
Shikhar Subedi <shikhar.subedi@Shikhar-Subedis-MacBook-Pro.local>
2019-07-04 16:35:32 -0700

    working except one bug

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let  result = 0
-let operation = undefined
+let prevOperation = undefined
 let numericArray = []
 let prevValue = undefined
 
@@ -10,12 +10,16 @@ container.addEventListener('click',function(event) {
         return
     }
     event.preventDefault()
-    const displayObject = getOutput(event.target.textContent)
-    render(displayObject)
+    const displayOutput = handleClick(event.target.textContent)
+    render(displayOutput)
     return
     
 })
-
+/**
+ * check if the button pressed is an operation
+ * @param {String} value the value of the button pressed
+ * @returns {Boolean}
+ */
 function isOperation(value) {
     const operations = ['x','÷','+', '-']
     return operations.includes(value)
@@ -24,16 +28,25 @@ function isNumber(value) {
     const numericStrings = [1,2,3,4,5,6,7,8,9,0].map(number => number.toString())
     return numericStrings.includes(value)
 } 
-
+/**
+ * check if the button pressed is the equal sign
+ * @param {String} value the value of the button pressed
+ * @returns {Boolean}
+ */
 function isEqualSign(value) {
     return value === '='
 }
 
+/**
+ * check if the button pressed is the clear button
+ * @param {String} value the value of the button pressed
+ * @returns {Boolean}
+ */
 function isClear(value) {
     return value === 'C'
 }
 
-function getOutput(value) {
+function handleClick(value) {
     
     if(isClear(value)) {
       return processClear()
@@ -44,28 +57,83 @@ function getOutput(value) {
     if(isNumber(value)) {
        return processNumber(value)
     }
-    if(isEqual(value)) {
-        return processEqualSign()
+    if(isEqualSign(value)) {
+        return processEqualSign(value)
     }
+}
+
+function processEqualSign(value) {
+    let operandString = numericArray.join("") || '0'
+    let operand =  Number(operandString)
+    if(!prevOperation) {
+        result = operand
+        prevValue = value
+        numericArray = []
+        return result
+    }
+    prevValue = value
+    result = executeOperation(prevOperation, result, operand)
+    numericArray = []
+    return result
 }
 
 function processClear() {
-    return '';
+    reset()
+    return result
 }
 
-function processNumber(value) {
-    if(isNumber(prevValue) || prevValue === undefined) {
-        numericArray.push(value)
-        prevValue = value
-        return numericArray.join("")
-    }
-    if(isOperation(prevValue)) {
+function reset() {
+    prevValue = undefined
+    prevOperation = undefined
+    numericArray = []
+    result = 0
+}
 
-    }
-
+function  processNumber(value) {
+    numericArray.push(value)
+    prevValue = value
+    return numericArray.join("")
 }
 
 function processOperation(operator) {
+    
+    let operandString = numericArray.join("") || '0'
+    let operand =  Number(operandString)
+    numericArray = []
+    
+    if(isEqualSign(prevValue)) {
+        prevValue = operator
+        return result
+    }
+    if(isOperation(prevValue)) {
+        prevOperation = operator
+        return result
+    }
+    if(!prevOperation) {
+        prevValue = operator
+        prevOperation = operator
+        result = operand
+        return result
+    }
+    prevValue = operator
+    result = executeOperation(prevOperation,result,operand)
+    prevOperation = operator
+    return result   
+}
+
+
+
+function executeOperation(operator, operand1, operand2) {
+    switch(operator) {
+        case '-':
+        return operand1 - operand2
+        case '+':
+            return operand1 + operand2
+        case '÷':
+            return operand1 / operand2
+        case 'x':
+            return operand1 * operand2
+    }
 }
 
 function createDisplayObject(content,clear) {
@@ -75,7 +143,11 @@ function createDisplayObject(content,clear) {
 function render(output) {
     const display = document.querySelector('.display')
     display.textContent = ""
-    
+    if(Number.isNaN(output)){
+        reset()
+        display.textContent = 'Not a number'
+        
+    }
     display.textContent= output
 }
 
